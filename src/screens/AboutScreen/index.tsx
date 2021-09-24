f26ed11d2d05a3402a1c956cade9e5fc96f3160b
Federico Giorgino <f_giorgino@icloud.com>
2021-09-24 19:15:09 +0200

    Placeholder Screens

---
+++ src/screens/AboutScreen/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import { View, Text } from "react-native";
+
+const AboutScreen: FC = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default AboutScreen;
